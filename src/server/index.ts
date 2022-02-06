77a090c202f4de6cea130dd85fc376523ae68e2e
BuGalter <wardoc78@gmail.com>
2022-02-06 19:49:22 +0300

    feat: Added connection to db in server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,21 @@
 import * as Hapi from '@hapi/hapi';
 import config from './config/config';
+import sequelize from './models';
 
 const createServer = async () => {
   const server = Hapi.server({
     port: config.server.port,
     host: config.server.host,
   });
+
+  try {
+    await sequelize.sync();
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  }
+  catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
   return server;
 };
