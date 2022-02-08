a81989dd77b5ea2a5d658a444c82bf0242ab80e4
BuGalter <wardoc78@gmail.com>
2022-02-08 21:35:06 +0300

    feat: Added to config lnk to provider, contract addres and privet key

--- src/server/config/config.ts
+++ src/server/config/config.ts
@@ -13,4 +13,13 @@ export default {
     port: process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3000,
     host: process.env.SERVER_HOST ? process.env.SERVER_HOST : 'localhost',
   },
+  contract: {
+    adress: process.env.CONTRACT_ADDRES
+  },
+  provider: {
+    link: process.env.PROVIDER_LINK
+  },
+  privatKey: {
+    myAccount: process.env.PRIVATE_KEY_ACCOUNT
+  },
 };
