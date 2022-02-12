e51241e55cb0a6bf40e70f06f591f857784570f7
BuGalter <wardoc78@gmail.com>
2022-02-12 18:52:34 +0300

    feat: Added import models to sequelize instance feat: Added logging false

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -1,9 +1,14 @@
 import { Sequelize, } from 'sequelize-typescript';
 import config from '../config/config';
+import { Acount, } from './Acount';
+import { Wallet, } from './Wallet';
+import { Transaction } from './Transaction';
 
 const sequelize = new Sequelize(config.db.dbName, config.db.userName, config.db.password, {
   host: config.db.dbHost,
   dialect: 'postgres',
+  models: [Acount, Wallet, Transaction],
+  logging: false,
 });
 
 export default sequelize;
