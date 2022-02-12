16186c933bdd2ad33798c1b6b30e45b802cd39e8
BuGalter <wardoc78@gmail.com>
2022-02-12 18:47:02 +0300

    feat: Added to server subscribes on events
    feat: Added handling previous deposit event
    feat: Added subscrube on deposit events
    feat: Added handling previous withdraw event
    feat: Added subscrube on withdraw events

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,8 @@
 import * as Hapi from '@hapi/hapi';
 import config from './config/config';
 import sequelize from './models';
+import { contract, handlingEventsDeposit, handlingEventsWithdraw, } from './contract';
+import { handleDepositEvent, handleWithdrawEvent, } from './utils/contract';
 
 const createServer = async () => {
   const server = Hapi.server({
@@ -16,6 +18,48 @@ const createServer = async () => {
   catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+
+  const eventsDeposit = await contract.getPastEvents('Deposit', { fromBlock: 0, toBlock: 'latest' });
+  const resultDeposit = await handlingEventsDeposit(eventsDeposit);
+  console.log(resultDeposit);
+
+  const depositSubscribe = await contract.events.Deposit({
+    fromBlock: 'latest',
+    }, 
+    async (err, event) => {
+      if (!err) {
+        console.log('Event')
+        let result = await handleDepositEvent(event);
+        console.log(result);
+      } else {
+        console.log(err);
+      }
+  });
+  
+  depositSubscribe.on('connected', (subscriptionId) => {
+    console.log(`Subscription event deposit connected, id: ${subscriptionId}`);
+  });
+
+  const eventsWithdraw = await contract.getPastEvents('Withdraw', { fromBlock: 0, toBlock: 'latest' });
+  const resultWithdraw = await handlingEventsWithdraw(eventsWithdraw);
+  console.log(resultWithdraw);
+  
+  const withdrawSubscribe = await contract.events.Withdraw({
+    fromBlock: 'latest',
+    }, 
+    async (err, event) => {
+      if (!err) {
+        console.log('Event')
+        let result = await handleWithdrawEvent(event);
+        console.log(result);
+      } else {
+        console.log(err);
+      }
+  });
+
+  withdrawSubscribe.on('connected', (subscriptionId) => {
+    console.log(`Subscription event withdraw connected, id: ${subscriptionId}`);
+  });
 
   return server;
 };
